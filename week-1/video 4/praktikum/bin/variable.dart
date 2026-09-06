48d67b215461e82020e7cac1a6f6bf789623dbe2
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-09-06 23:01:58 +0700

    tipe data dart

---
+++ week-1/video 4/praktikum/bin/variable.dart
@@ -0,0 +1,14 @@
+import 'package:praktikum/praktikum.dart' as praktikum;
+void main(List<String> arguments) {
+  int a = 10;
+  double b = 3.14;
+  String c = 'Hello, Dart!';
+  bool d = true;
+
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+
+  
+}
