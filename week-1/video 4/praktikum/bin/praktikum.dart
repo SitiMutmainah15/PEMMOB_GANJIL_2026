0cfae2c3eb82fa6adf832ec42e9acca0eec622b7
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-09-06 22:52:53 +0700

    mencoba  final

--- week-1/video 4/praktikum/bin/praktikum.dart
+++ week-1/video 4/praktikum/bin/praktikum.dart
@@ -2,6 +2,9 @@ import 'package:praktikum/praktikum.dart' as praktikum;
 const String name = 'Iin';
 const int age = 20;
 void main(List<String> arguments) {
-  const String name = "Siti";
+  //const String name = "Siti"; 
+  final String nama = 'Iin';
+  final int umur = 21;
   print('Nama saya $name, umur saya $age tahun');
+  print('Nama saya $nama, umur saya $umur tahun');
 }
